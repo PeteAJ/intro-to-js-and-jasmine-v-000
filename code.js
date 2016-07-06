0a96e0cbc1b3a57def0617ae4ea911c63cac185d
Peter Jacobson <github email address>
2016-07-06 18:09:15 -0400

    Done.

--- code.js
+++ code.js
@@ -1,1 +1,15 @@
-'use strict';
+function favIceCream(name_of_icecream) {
+  return "I love " + name_of_icecream;
+}
+
+function shouting(string) {
+  return string.toUpperCase();
+}
+
+function roundDown(number) {
+  return Math.floor(number);
+}
+
+function theTruth() {
+  return true;
+}
